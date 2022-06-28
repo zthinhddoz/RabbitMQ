5491c29819c445b5c10a21d2fdd6c1cd617bf18c
Thinh Duy Do <doduythinh.1999@gmail.com>
2022-06-28 10:09:56 +0700

    update env config

--- create-images.sh
+++ create-images.sh
@@ -5,10 +5,11 @@
 # This script will auto create all needed image for shinev2 product deployment
 
 # Create shine server image
-docker build -t shinev2_server -f ./server/docker/create.Dockerfile .
+# docker build -t shinev2_server -f ./server/docker/create.Dockerfile .
+docker build -t doex-service -f ./server/Dockerfile .
 
 # Create shine cdn image
-docker build -t shinev2_cdn -f ./gateway/cdn/Dockerfile .
+# docker build -t shinev2_cdn -f ./gateway/cdn/Dockerfile .
 
 # Create shine database image
-docker build -t shinev2_db -f ./database/Dockerfile .
+# docker build -t shinev2_db -f ./database/Dockerfile .
