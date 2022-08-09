814a325b811955a96398ae06e6d5112d9572ec6b
Thinh Duy Do <doduythinh.1999@gmail.com>
2022-08-09 15:29:50 +0700

    update docker compose config

--- create-images.sh
+++ create-images.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 # Author:       ManhVu
-# Date:         2021-Oct-13
+# Date:         2022-June-xx
 # Usage:        ./create-images.sh
 # This script will auto create all needed image for shinev2 product deployment
 
 # Create shine server image
-# docker build -t shinev2_server -f ./server/docker/create.Dockerfile .
-docker build -t doex-service -f ./server/Dockerfile .
-
-# Create shine cdn image
-# docker build -t shinev2_cdn -f ./gateway/cdn/Dockerfile .
-
-# Create shine database image
-# docker build -t shinev2_db -f ./database/Dockerfile .
+docker build -t queue-list-service -f ./server/Dockerfile .
--- database/README.md
+++
@@ -1,9 +0,0 @@
-# Docker build command
-
-```
-cd {WORK_DIR_OF_DOCKER_FILE}
-docker build . -t <docker_image_name>:<tag>
-
-- Ex: docker build . -t shinev2_db
-
-```
